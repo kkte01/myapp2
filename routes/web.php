36e0073d71ae27ea43ac7d4b0a39577e8a3055a4
kkw <kkte01@naver.com>
2020-12-23 16:06:50 +0900

    라우팅 연습

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/', function () {
+//    //return view('welcome');
+//    return '<h1>Hello Foo</h1>';
+//});
+
+// 코드 3-5 app/Http/routes.php
+//Route::get('/{foo?}', function ($foo = 'bar') {
+//    return $foo;
+//});
+// 코드 3-6 app/Http/routes.php
+//Route::pattern('foo', '[0-9a-zA-Z]{3}');
+//
+//Route::get('/{foo?}', function ($foo = 'bar') {
+//    return $foo;
+//});
+// 코드 3-7 app/Http/routes.php
+//Route::get('/{foo?}', function ($foo = '기본 값') {
+//    return $foo;
+//})->where('foo', '[0-9a-zA-Z]{3}')->name('foo');
+// 코드 3-8 app/Http/routes.php
 Route::get('/', function () {
-    return view('welcome');
+        return '제 이름은 "home" 입니다.';
+    })->name('home');
+
+Route::get('/home', function () {
+    return redirect(route('home'));
 });

e2434777c608a553d77fbcc80ff84a0bb3483ecd
kkw <kkte01@naver.com>
2020-12-28 09:51:21 +0900

    데이터 베이스 마이그레이션 연습

---
+++ database/migrations/2020_12_27_055821_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->comment('제목');
+            $table->text('body')->comment('본문');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}

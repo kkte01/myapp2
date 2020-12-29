69affd94f75f4761960ff98ff17a591996fafd4a
kkw <kkte01@naver.com>
2020-12-29 13:46:35 +0900

    엘로퀀트 ORM 연습

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,7 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+    public function articles(){
+        return $this->hasMany(Article::class);
+    }
 }
---
+++ database/migrations/2020_12_29_005744_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->string('title')->comment('제목');
+            $table->text('content')->comment('내용');
+            $table->timestamps();
+
+            //외래키 설정
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
